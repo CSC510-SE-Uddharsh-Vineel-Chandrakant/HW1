2bfbef0479ac1a5660b488304f59b7f2f3e20b2a
vineel31 <64918224+vineel31@users.noreply.github.com>
2024-08-27 17:37:27 +0000

    Testing coverage badge

--- tests/test_myfile1.py
+++ tests/test_myfile1.py
@@ -4,4 +4,4 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from myfile import armstrong_number
 
 def test_armstrong_number_fail():
-    assert armstrong_number(531)
+    assert armstrong_number(153)
