bda23e6e0a40527a53f60a03f83077a1f8660492
vineel31 <64918224+vineel31@users.noreply.github.com>
2024-08-27 17:15:20 +0000

    Rollback

--- myfile.py
+++ myfile.py
@@ -2,10 +2,15 @@ def armstrong_number(number):
     s=number
     a=len(str(number))
     sum = 0
-    
     while number != 0:
         res = number % 10
         sum = sum+(res**a)
         number //= 10
-
     return s == sum
+
+if __name__ == "__main__":
+    number = "531"
+    if armstrong_number(number):
+        print("The given number", number, "is an Armstrong number")
+    else:
+        print("The given number", number, "is not an Armstrong number")
