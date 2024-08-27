0aff0a64f6dd859302f6cd0766c8fc4504b28091
vineel31 <64918224+vineel31@users.noreply.github.com>
2024-08-27 02:09:24 +0000

    Modified Tests

--- test_myfile1.py
+++
@@ -1,4 +0,0 @@
-from myfile import armstrong_number
-
-def test_armstrong_number_fail():
-    assert armstrong_number(531)
--- tests/test_myfile.py
+++ tests/test_myfile.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from myfile import armstrong_number
 
 def test_armstrong_number_pass():
