c0f16b8b0ca50444219c088d22a84efdbf510c4d
Chandrakant Koneti <80403832+konetichandrakant@users.noreply.github.com>
2024-08-27 02:54:32 +0000

    Removed unwanted code from myfile

--- myfile.py
+++ myfile.py
@@ -2,15 +2,10 @@ def armstrong_number(number):
     s=number
     a=len(str(number))
     sum = 0
+    
     while number != 0:
         res = number % 10
         sum = sum+(res**a)
         number //= 10
-    return s == sum
 
-if __name__ == "__main__":
-    number = "531"
-    if armstrong_number(number):
-        print("The given number", number, "is an Armstrong number")
-    else:
-        print("The given number", number, "is not an Armstrong number")
+    return s == sum
