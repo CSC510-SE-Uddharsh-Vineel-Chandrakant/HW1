4090a5333f2d6064df372111808e839611889b7d
Chandrakant Koneti <80403832+konetichandrakant@users.noreply.github.com>
2024-09-11 03:29:56 +0000

    Static analysis using methods and pre_traces updated

--- hw2/hw2_debugging.py
+++ hw2/hw2_debugging.py
@@ -1,58 +1,35 @@
-"""
-This Module is for implementing the merge sort algorithm.
-"""
-
 import rand
 
-def merge_sort(arr):
-    """
-    This function performs the merge sort.
-    Args:
-        arr(list): The array to sort.
-    Returns:
-        list: A sorted version of the input array.
-    """
-    if len(arr) == 1:
+def mergeSort(arr):
+    if (len(arr) == 1):
         return arr
 
-    half = len(arr) // 2
-    return recombine(merge_sort(arr[:half]), merge_sort(arr[half:]))
+    half = len(arr)//2
 
-def recombine(left_arr, right_arr):
-    """
-    This function Combines two sorted arrays into a single sorted array.
+    return recombine(mergeSort(arr[:half]), mergeSort(arr[half:]))
 
-    Args:
-        left_arr(list): The left half of the array.
-        right_arr(list): The right half of the array.
-
-    Returns:
-        list: A merged sorted array.
-    """
-    left_index = 0
-    right_index = 0
-    merged_array = [None] * (len(left_arr) + len(right_arr))
-
-    while left_index < len(left_arr) and right_index < len(right_arr):
-        if left_arr[left_index] < right_arr[right_index]:
-            merged_array[left_index + right_index] = left_arr[left_index]
-            left_index += 1
+def recombine(leftArr, rightArr):
+    leftIndex = 0
+    rightIndex = 0
+    mergeArr = [None] * (len(leftArr) + len(rightArr))
+    while leftIndex < len(leftArr) and rightIndex < len(rightArr):
+        if leftArr[leftIndex] < rightArr[rightIndex]:
+            rightIndex += 1
+            mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
         else:
-            merged_array[left_index + right_index] = right_arr[right_index]
-            right_index += 1
+            leftIndex += 1
+            mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
 
-    for i in range(right_index, len(right_arr)):
-        merged_array[left_index + right_index] = right_arr[i]
-        right_index += 1
+    for i in range(rightIndex, len(rightArr)):
+        mergeArr[leftIndex + rightIndex] = rightArr[i]
 
-    for i in range(left_index, len(left_arr)):
-        merged_array[left_index + right_index] = left_arr[i]
-        left_index += 1
+    for i in range(leftIndex, len(leftArr)):
+        mergeArr[leftIndex + rightIndex] = leftArr[i]
 
-    return merged_array
+    return mergeArr
 
-
-array = rand.random_array([None] * 20)
-arr_out = merge_sort(array)
+arr = rand.random_array([None] * 20)
+arr_out = mergeSort(arr)
 
 print(arr_out)
+
--- hw2/rand.py
+++ hw2/rand.py
@@ -1,21 +1,8 @@
-"""
-This module is for generating a random array of numbers.
-"""
-
 import subprocess
 
 def random_array(arr):
-    """
-    Produces a random array by shuffling the numbers between 1 and 20.
-
-    Args:
-        arr (list): A list with 0 elements to be filled with random numbers.
-
-    Returns:
-        list: A list filled with random numbers.
-    """
-    for i, _ in enumerate(arr):
-        shuffled_num = subprocess.run(
-            ["shuf", "-i1-20", "-n1"], capture_output=True, check=True)
+    shuffled_num = None
+    for i in range(len(arr)):
+        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
         arr[i] = int(shuffled_num.stdout)
     return arr
