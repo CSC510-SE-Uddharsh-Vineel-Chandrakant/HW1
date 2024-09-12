6eff85a46855cff0923ebc83264a7602656980bf
Uddharsh Vasili <Uddharsh.v19@gmail.com>
2024-09-12 20:48:24 +0000

    Modified regression check test file

--- hw2/test_regressioncheck.py
+++ hw2/test_regressioncheck.py
@@ -1,3 +1,4 @@
-def test_my_function(regression):
-    result = my_function()
+from hw2_debugging import merge_sort
+def test_merge_sort(regression):
+    result = merge_sort([5, 2, 9, 1, 5, 6])
     regression.check(result)
